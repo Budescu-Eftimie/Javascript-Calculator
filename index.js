8fa40b045046cf07497f206b9798ddf76092774e
Budescu Eftimie <budescueftimie@gmail.com>
2021-12-25 05:17:54 +0100

    adding reset display functionality

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// variables
+//const resetBtn = document.body.querySelector('button[data-key="C"]');
+let display = document.getElementsByClassName("screen")[0];
+const buttons = document.body.querySelectorAll("button");
+
+function doSomething() {
+  let btnClicked = this.getAttribute("data-key");
+  if (btnClicked === "C") {
+    display.textContent = 0;
+  }
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", doSomething);
+});
+
+//operations
 let add = (a, b) => {
   return a + b;
 };
